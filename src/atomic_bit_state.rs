e2e7c7500cd095b96d389fd65148042c6c458238
skandrk <skandrk@outlook.com>
2025-10-28 01:16:42 +0530

    Atomic

--- src/atomic_bit_state.rs
+++ src/atomic_bit_state.rs
@@ -1,38 +1,112 @@
-use crate::BitState;
 use std::sync::atomic::{AtomicU8, AtomicU16, AtomicU32, AtomicU64, Ordering};
+
+pub trait AtomicBitState<T, F> {
+    fn set(&self, new: F);
+    fn set_with_changes(&self, new: F) -> Option<(Vec<u8>, Vec<u8>)>;
+    fn set_bit(&self, n: u8);
+    fn clear_bit(&self, n: u8);
+    fn get_bit(&self, n: u8) -> bool;
+    fn reset(&self);
+}
 
 macro_rules! bit_state_impl_atomics {
   ($($ty:ident : $fr:ident),+) => {
     $(
-      impl BitState<$ty,$fr> for $ty{
+      impl AtomicBitState<$ty,$fr> for $ty {
 
-        fn set(&mut self, new: $fr){
-          self.swap(new,Ordering::SeqCst);
+        #[inline]
+        fn set(&self, new: $fr) {
+          self.store(new,Ordering::Relaxed);
+        }
+
+        fn set_with_changes(&self, new: $fr) -> Option<(Vec<u8>, Vec<u8>)>{
+
+          let mut old = self.load(Ordering::Relaxed);
+
+          loop{
+            if old == new {
+              return None;
+            }
+
+            match self.compare_exchange_weak(
+              old,
+              new,
+              Ordering::Relaxed,
+              Ordering::Relaxed
+              ) {
+                  Ok(_) => break,
+                  Err(x) => old = x, // Retry with updated value
+              }
+          }
+
+          let mut up_bits = (old ^ new) & new;
+          let mut down_bits = (old ^ new) & (!new);
+          let mut pos: u8 = 0;
+
+          let mut ups = Vec::new();
+          let mut downs = Vec::new();
+
+          while (up_bits != 0 || down_bits !=0) {
+            if(up_bits & 1 == 1){
+              ups.push(pos);
+            }
+            if(down_bits & 1 == 1){
+              downs.push(pos);
+            }
+            up_bits >>= 1;
+            down_bits >>= 1;
+            pos += 1;
+          }
+
+          Some((ups,downs))
         }
 
         #[inline]
-        fn set_bit(&mut self, n: u8) {
-          self.fetch_or(1 << n, Ordering::SeqCst);
+        fn set_bit(&self, n: u8) {
+          self.fetch_or(1 << n, Ordering::Relaxed);
         }
 
         #[inline]
-        fn clear_bit(&mut self, n: u8) {
-          self.fetch_and(!(1 << n), Ordering::SeqCst);
+        fn clear_bit(&self, n: u8) {
+          self.fetch_and(!(1 << n), Ordering::Relaxed);
         }
 
         #[inline]
         fn get_bit(&self, n: u8) -> bool {
-          (self.load(Ordering::SeqCst) & (1 << n)) != 0
+          (self.load(Ordering::Relaxed) & (1 << n)) != 0
         }
 
         #[inline]
-        fn reset(&mut self) {
-          self.store(0, Ordering::SeqCst);
+        fn reset(&self) {
+          self.store(0, Ordering::Relaxed);
         }
-
       }
     )+
   };
 }
 
 bit_state_impl_atomics!(AtomicU8 : u8, AtomicU16 : u16, AtomicU32 : u32, AtomicU64 : u64);
+
+#[cfg(test)]
+#[cfg(feature = "atomic")]
+mod tests {
+    use crate::AtomicBitState;
+    use std::sync::Arc;
+    use std::sync::atomic::AtomicU8;
+
+    #[test]
+    fn atomic_u8() {
+        let result = Arc::new(AtomicU8::new(0));
+        result.set_bit(2);
+        assert_eq!(
+            result.load(std::sync::atomic::Ordering::Acquire),
+            0b_0000_0100
+        );
+        result.set_bit(3);
+        result.clear_bit(2);
+        assert_eq!(
+            result.load(std::sync::atomic::Ordering::Acquire),
+            0b_0000_1000
+        );
+    }
+}
--- src/bit_state.rs
+++ src/bit_state.rs
@@ -1,5 +1,6 @@
-pub trait BitState<T, F> {
-    fn set(&mut self, new: F);
+pub trait BitState<T> {
+    fn set(&mut self, new: T);
+    fn set_with_changes(&mut self, new: T) -> Option<(Vec<usize>, Vec<usize>)>;
     fn set_bit(&mut self, n: u8);
     fn clear_bit(&mut self, n: u8);
     fn get_bit(&self, n: u8) -> bool;
@@ -8,10 +9,37 @@ pub trait BitState<T, F> {
 
 macro_rules! bit_state_impl {
   ($($t: ident ),+) => {
-       $( impl BitState<$t, $t> for $t {
+       $(impl BitState<$t> for $t {
 
-            fn set(&mut self, new: $t){
+            #[inline]
+            fn set(&mut self, new: $t) {
               *self = new;
+            }
+
+            fn set_with_changes(&mut self, new: $t) -> Option<(Vec<usize>,Vec<usize>)> {
+              if *self == new {
+                return None;
+              };
+              let mut up_bits = (*self ^ new) & new;
+              let mut down_bits = (*self ^ new) & (!new);
+              let mut pos = 0;
+
+              let mut ups = Vec::new();
+              let mut downs = Vec::new();
+
+              while (up_bits != 0 || down_bits !=0) {
+                if(up_bits & 1 == 1){
+                  ups.push(pos);
+                }
+                if(down_bits & 1 == 1){
+                  downs.push(pos);
+                }
+                up_bits >>= 1;
+                down_bits >>= 1;
+                pos += 1;
+              }
+              *self = new;
+              Some((ups,downs))
             }
 
             #[inline]
@@ -38,3 +66,17 @@ macro_rules! bit_state_impl {
 }
 
 bit_state_impl!(u8, u16, u32, u64, u128);
+
+#[cfg(test)]
+mod tests {
+    use crate::BitState;
+
+    #[test]
+    fn it_works() {
+        let mut result = 0u8;
+        result.set_bit(1);
+        assert_eq!(result, 0b_0000_0010);
+        result.set_bit(3);
+        assert_eq!(result, 0b_0000_1010);
+    }
+}
