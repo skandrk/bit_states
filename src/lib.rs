4b645ca02830dcab777560f6eb7008689677c9e5
skandrk <skandrk@outlook.com>
2025-10-31 09:41:24 +0530

    Atomic Flags

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
-#[cfg(feature = "atomic")]
-pub mod atomic_bit_state;
+#[cfg(feature = "non-atomic")]
+pub use bit_state::BitState;
 
 #[cfg(feature = "non-atomic")]
 pub mod bit_state;
@@ -10,5 +10,8 @@ pub mod bit_flags;
 #[cfg(feature = "atomic")]
 pub use atomic_bit_state::AtomicBitState;
 
-#[cfg(feature = "non-atomic")]
-pub use bit_state::BitState;
+#[cfg(feature = "atomic")]
+pub mod atomic_bit_state;
+
+#[cfg(feature = "atomic")]
+pub mod atomic_bit_flags;
