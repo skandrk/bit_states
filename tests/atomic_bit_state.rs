6963197804790d7a21d8eb03342feff0b13898df
skandrk <skandrk@outlook.com>
2025-11-05 18:50:54 +0530

    Added tests for atomic helpers

--- tests/atomic_bit_state.rs
+++ tests/atomic_bit_state.rs
@@ -45,3 +45,37 @@ fn atomics() {
     assert!(events_up.lock().unwrap().contains(&Status::Four));
     assert!(events_down.lock().unwrap().contains(&Status::Zero));
 }
+
+#[test]
+fn atomic_helpers() {
+    let events_up: Arc<Mutex<Vec<Status>>> = Arc::new(Mutex::new(Vec::new()));
+    let events_down: Arc<Mutex<Vec<Status>>> = Arc::new(Mutex::new(Vec::new()));
+
+    let set_status = std::sync::Arc::new(StatusAtomicStates::new(
+        {
+            let events_up = events_up.clone();
+            move |a| events_up.lock().unwrap().push(a)
+        },
+        {
+            let events_down = events_down.clone();
+            move |a| events_down.lock().unwrap().push(a)
+        },
+    ));
+
+    let j = std::thread::spawn({
+        let set_status = set_status.clone();
+        move || {
+            set_status.set(0b_0001_0000 as u8);
+            set_status.set_flag(Status::Three);
+        }
+    });
+
+    j.join().unwrap();
+    assert_eq!(set_status.get(), 0b_0001_1000 as u8);
+    assert_eq!(events_up.lock().unwrap().pop(), Some(Status::Three));
+    assert_eq!(set_status.is_set(Status::Three), true);
+    set_status.clear();
+    assert_eq!(set_status.get(), 0);
+    set_status.reset_flag(Status::Three);
+    assert_eq!(events_down.lock().unwrap().len(), 0);
+}
